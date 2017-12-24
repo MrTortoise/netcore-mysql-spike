48bea22f7d9b8406c0a96d2163d591d599bdb0d6
John Nicholas <themrtortoise@gmail.com>
2017-12-24 19:03:53 +0000

    initial

---
+++ MySqlSpike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySqlSpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
